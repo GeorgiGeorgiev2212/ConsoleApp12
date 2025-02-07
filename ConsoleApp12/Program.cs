2b9b200b42f83a6a29175a215396ac4ada5eb303
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-07 15:17:42 +0200

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,32 @@
+﻿//kalkulator za vuzrast za katerushki
+int age = int.Parse(Console.ReadLine());
+
+if (age <= 3)
+{
+
+    Console.WriteLine("Ne moje priqtelche");
+
+
+}
+
+else if (age <= 7)
+{
+
+
+    Console.WriteLine("Stava samo s pridrujitel");
+
+
+
+
+
+}
+
+
+else if (age <= 1000000000000)
+{
+
+    Console.WriteLine("Stava");
+
+
+
+}
